084b24dbc274c5774d3f9142d85799156a967aad
goaldaika <61223820+goaldaika@users.noreply.github.com>
2024-08-28 12:08:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ onload = () => {
       document.body.classList.remove("not-loaded");
       clearTimeout(c);
     }, 1000);
-    alert("I wish your beauty will rivals Aphrodite and The Mirror will always chants your name. For the most importantly, I wish the only lethality that would ever happened to you is old age, and you will always be better than yourself yesterday. Happy Birthday Mai Trang.");
+   alert("I wish your beauty will rival Aphrodite and The Mirror will always chant your name.\n\nMost importantly, I wish the only lethality that would ever happen to you is old age, and you will always be better than yourself yesterday.\n\nHappy Birthday Mai Trang.");
   };
